d2c125b12e523075e10d6b724a2e668d6456e0df
Niklas <59961104+Fridoulin@users.noreply.github.com>
2021-11-13 20:02:07 +0100

    ohne if

--- SchereSteinPapier/main.py
+++ SchereSteinPapier/main.py
@@ -1,18 +1,51 @@
 import random
 
+
+def inputUser(valuableList):
+    choice = input("Rock, Paper, Scissors, Lizard, Spock: ").lower()
+    if choice in valuableList:
+        print("Your Choice: ", choice)
+        return choice
+    else:
+        print("Try again")
+        main()
+
+
+def inputComputer(valuableList):
+    choice = random.choice(valuableList)
+    print("Computers choice: ", choice)
+    return choice
+
+
+def check(choiceUser, choiceComputer):
+    guessToIndex = {"rock": 0, "spock": 1, "paper": 2, "lizard": 3, "scissors": 4}
+    user = guessToIndex.get(choiceUser)
+    pc = guessToIndex.get(choiceComputer)
+    return (user - pc) % 5
+
+
+def winner(result):
+    if result == 0:
+        print("Draw")
+    elif result == 1 or result == 2:
+        print("Won")
+    elif result > 2:
+        print("Lost")
+    print("\n")
+
+
+def main():
+    valuableList = ["rock", "spock", "paper", "lizard", "scissors"]
+    choiceUser = inputUser(valuableList)
+    choiceComputer = inputComputer(valuableList)
+    result = check(choiceUser, choiceComputer)
+    winner(result)
+
+
 while True:
-    player = input("schere, stein, papier: ").lower()
-    choice = ["schere", "stein", "papier"]
-    computer = random.choice(choice)
-    print("Spieler:", player)
-    print("Computer:", computer)
-    guess_index = {'schere': 0, 'stein': 1, 'papier': 2}
-    guess_player = guess_index.get(player, 3)
-    guess_computer = guess_index.get(computer)
-    possible_results = [[0, 2, 1], [1, 0, 2], [2, 1, 0], [3, 3, 3]]
-    result_compare = possible_results[guess_player][guess_computer]
-    result_messages = ["Unentschieden", "Gewonnen", 'Verloren', 'Ungültige Eingabe']
-    print(result_messages[result_compare])
-    again = input("Nochmal [j/n]? ")
-    if again != "j":
+    if __name__ == "__main__":
+        main()
+
+    again = input("Try again [y,n]? ")
+    if again == "n":
         break
