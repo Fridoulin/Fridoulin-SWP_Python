0f20a6ca672a78c784e089debd990ed36cee6b4d
Niklas <59961104+Fridoulin@users.noreply.github.com>
2021-12-15 16:05:55 +0100

    Update main.py

--- Firmen/main.py
+++ Firmen/main.py
@@ -1,76 +1,79 @@
 import firma as f
 
+Person = []
+Mitarbeiter = []
+Gruppenleiter = []
 
-class Company:
+def add_Person():
+    return (f.Person(f.Sex.Mann, "Jochen", "Scherzer", f.Abteilung.einkauf),
+    f.Person(f.Sex.Mann, "Konstantin", "Bauschinger", f.Abteilung.einkauf),
+    f.Person(f.Sex.Frau, "Maria", "Rosenblum", f.Abteilung.produktion),
+    f.Person(f.Sex.Frau, "Helge", "Werkner", f.Abteilung.verkauf),
+    f.Person(f.Sex.Mann, "Reiner", "Adelman", f.Abteilung.produktion),
+    f.Person(f.Sex.Mann, "Justin", "Bodenheimer", f.Abteilung.verkauf),
+    f.Person(f.Sex.Mann, "Phillip", "Strassmann", f.Abteilung.produktion),
+    f.Person(f.Sex.Mann, "Antonin", "Tendler", f.Abteilung.einkauf))
 
-    def __init__(self):
-        self.persons = []
-        self.department_counters = {}
-        [self.department_counters.setdefault(i, 0) for i in f.Abteilung]
+def add_Mitarbeiter():
+    return (f.Mitarbeiter(f.Sex.Mann, "Sebastian", "Rimml", f.Abteilung.einkauf, 7541),
+    f.Mitarbeiter(f.Sex.Mann, "Manuel", "Repetschnig", f.Abteilung.produktion, 7695),
+    f.Mitarbeiter(f.Sex.Mann, "David", "Seidl", f.Abteilung.produktion, 7864),
+    f.Mitarbeiter(f.Sex.Frau, "Helga", "Santler", f.Abteilung.verkauf, 1452),
+    f.Mitarbeiter(f.Sex.Frau, "Sofia", "Santner", f.Abteilung.einkauf, 4566),
+    f.Mitarbeiter(f.Sex.Mann, "David", "Simma", f.Abteilung.einkauf, 5978),
+    f.Mitarbeiter(f.Sex.Frau, "Daniel", "Niederhauser", f.Abteilung.einkauf, 7878))
 
-    def add_person(self, p: f.Person):
-        if not isinstance(p, f.Person):
-            raise Exception()
-        self.persons.append(p)
-        self.department_counters[p.Abteilung] += 1
+def add_Gruppenleiter():
+    return (f.Gruppenleiter(f.Sex.Mann,"Torben", "Ullmer", f.Abteilung.einkauf, 9512, 1),
+    f.Gruppenleiter(f.Sex.Mann, "Kaleb", "Halberstam", f.Abteilung.produktion, 9365, 2),
+    f.Gruppenleiter(f.Sex.Frau, "Tina", "Strauss", f.Abteilung.verkauf, 9874, 3))
 
-    def add_persons(self, l: list):
-        for p in l:
-            self.add_person(p)
+def get_Abteilung():
+    return len(f.Abteilung)
 
-    def allePersonen(self):
-        return self.persons
+def get_Anzahl():
+    return len(Mitarbeiter), len(Gruppenleiter)
 
-    def alleMitarbeiter(self):
-        ms = []
-        for m in self.persons:
-            if isinstance(m, f.Mitarbeiter):
-                ms.append(m)
-        return ms
+def get_MannZuFrauAnteil():
+    mann = 0
+    for p in Person:
+        if p.Sex == f.Sex.Mann:
+            mann += 1
+    for m in Mitarbeiter:
+        if m.Sex == f.Sex.Mann:
+            mann += 1
+    for g in Gruppenleiter:
+        if g.Sex == f.Sex.Mann:
+            mann += 1
+    print("X", mann)
+    return "%: " + str(round(mann / (len(Person) + len(Mitarbeiter) + len(Gruppenleiter)), 2) * 100) + " / " + str(round(
+        (len(Person) + len(Mitarbeiter) + len(Gruppenleiter) - mann) / (
+                len(Person) + len(Mitarbeiter) + len(Gruppenleiter)), 2) * 100)
 
-    def alleGruppenleiter(self):
-        gs = []
-        for g in self.persons:
-            if isinstance(g, f.Gruppenleiter):
-                gs.append(g)
-        return gs
+def get_Meisten_Mitarbeiter(ab):
+    anzahl_Mitarbeiter = 0
+    for p in Person:
+        if p.Abteilung == ab:
+            anzahl_Mitarbeiter += 1
 
-    def getAbteilungen(self):
-        return f.Abteilung
+    for m in Mitarbeiter:
+        if m.Abteilung == ab:
+            anzahl_Mitarbeiter += 1
 
-    def meistenArbeiter(self):
-        return max(self.department_counters, key=self.department_counters.get)
+    for g in Gruppenleiter:
+        if g.Abteilung == ab:
+            anzahl_Mitarbeiter += 1
 
-    def getGender(self, s: f.Sex):
-        if not isinstance(s, f.Sex):
-            raise Exception()
-        gs = []
-        for g in self.persons:
-            if g.Sex == s:
-                gs.append(g)
-        return gs
-
-    def prozentMaennerzuFrauen(self, s: f.Sex):
-        return 100 * len(self.getGender(s)) / len(self.persons)
-
-    def getArbeiterNachAbteilungen(self, abteilung: f.Abteilung):
-        if not isinstance(abteilung, f.Abteilung):
-            raise Exception()
-        es = []
-        for e in self.persons:
-            if e.abteilung == abteilung:
-                es.append(e)
-        return es
-
-def ausgabe():
-    print(f'Alle Personen: ',len(c.allePersonen()) ,f' \nAlle Mitarbeiter:',len(c.alleMitarbeiter()),'\nAlle Gruppenleiter',len(c.alleGruppenleiter()))
-    print(f'Alle Abteilungen: ',len(c.getAbteilungen()))
-    print(f'Abteilung mit den meisten Mitarbeitern: ',c.meistenArbeiter())
-    print(f'Prozent von Männern zu Frauen: ',c.prozentMaennerzuFrauen(f.Sex.Frau),'%')
+    return anzahl_Mitarbeiter
 
 if __name__ == '__main__':
-    employees = [f.Person(), f.Person(), f.Person(), f.Mitarbeiter(), f.Mitarbeiter(), f.Mitarbeiter(),
-                 f.Gruppenleiter(), f.Gruppenleiter(), f.Gruppenleiter()]
-    c = Company()
-    c.add_persons(employees)
-    ausgabe()
+    Person = add_Person()
+    Mitarbeiter = add_Mitarbeiter()
+    Gruppenleiter = add_Gruppenleiter()
+    print("Mitarbeiter: " + str(get_Anzahl()[0]))
+    print("Gruppenleiter: " + str(get_Anzahl()[1]))
+    print("Anzahl Abteilungen: " + str(get_Abteilung()))
+    print("Gesamtanzahl Mitarbeiter: ")
+    for d in f.Abteilung:
+        print(str(d._name_) + " " + str(get_Meisten_Mitarbeiter(d)))
+    print(get_MannZuFrauAnteil())
